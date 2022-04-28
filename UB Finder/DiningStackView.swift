2eb9a4ec13b7f168f51512ce78646defcc4185ba
EricXieQ <97911585+EricXieQ@users.noreply.github.com>
2022-04-28 00:50:14 -0400

    working rating

--- UB Finder/DiningStackView.swift
+++ UB Finder/DiningStackView.swift
@@ -12,11 +12,12 @@ class DiningStackView: UIStackView {
     var starsRating = 0
     var starsEmptyPicName = "star"
     var starsFilledPicName = "Filled Star"
+    var status = false
     
     override func draw(_ rect: CGRect) {
         
         let myViews = self.subviews.filter{$0 is UIButton}
-        var starTag = 1
+        var starTag = 0
         
         for theView in myViews {
             
@@ -24,8 +25,8 @@ class DiningStackView: UIStackView {
                 
                 theButton.setImage(UIImage(named: starsEmptyPicName), for: .normal)
                 theButton.addTarget(self, action: #selector(self.pressed(sender:)), for: .touchUpInside)
-                theButton.tag = starTag
                 starTag = starTag + 1
+                theButton.tag = starTag
             }
         }
     }
@@ -36,19 +37,27 @@ class DiningStackView: UIStackView {
         starsRating = sender.tag
         let myViews = self.subviews.filter{$0 is UIButton}
         for theView in myViews {
-            
+
             if let theButton = theView as? UIButton{
                 
                     
                     if theButton.tag > sender.tag{
                         
                         theButton.setImage(UIImage(named: starsEmptyPicName), for: .normal)
- 
+                        
+                        
+//
+                    }else if theButton.tag == 1 && theButton.tag == sender.tag && status == true {
+
+                        theButton.setImage(UIImage(named: starsEmptyPicName), for: .normal)
+                        status = false
+                        starsRating = 0
+                        
                     }else{
                     
                         theButton.setImage(UIImage(named: starsFilledPicName), for: .normal)
-                            
-                }
+                        status = true
+                    }
             }
         }
     }
