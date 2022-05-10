ce72bdad14e7e127ad8fff4c9da873b7b8831f8a
EricXieQ <97911585+EricXieQ@users.noreply.github.com>
2022-05-10 01:17:49 -0400

    dining and study view controller done with scaling

--- UB Finder/studyViewController.swift
+++ UB Finder/studyViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import Parse
 
-class studyViewController: UIViewController {
+class studyViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var studyTableView: UITableView!
+    
+    var locName = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        studyTableView.delegate = self
+        studyTableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let query = PFQuery(className: "studyPlace")
+        query.includeKey("locationPlace")
+        query.limit = 20
+        
+        query.findObjectsInBackground { locName, error in
+            
+            if locName != nil {
+                
+                self.locName = locName!
+                self.studyTableView.reloadData()
+                
+            }
+        }
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return locName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studyCell") as! studyCell
+        
+        let post = locName[indexPath.row]
+        
+        cell.locationName.text = (post["locationPlace"] as! String)
+        
+        return cell
+        
     }
     
 
